dcf56ea2101f359ca2fec323265c7b5ed24521f7
Naitik Patel <naitik2002patel@gmail.com>
2022-12-09 15:35:40 +0530

    update DB name in code

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://naitikp2002:Naitik@123@mern.nczhkbo.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
+  const collection = client.db("MERN").collection("friends");
   // perform actions on the collection object
   app.listen(PORT, () => {
     console.log("listening on port");
